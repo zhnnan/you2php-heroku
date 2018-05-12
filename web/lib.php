c660b8107b3a6224a7f8fb1a76cff01c2f78a14c
gfw-breaker <lixiaoming0513@gmail.com>
2018-05-12 10:34:58 +0800

    ok

--- web/lib.php
+++ web/lib.php
@@ -55,27 +55,16 @@ function get_related_video($vid,$apikey){
 
 //获取播放列表视频
 function get_playlist_video($plid,$apikey){
-	$data = getcache($plid);
-	if(!empty($data)){
-		return $data;
-	}
-   $apilink='https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&type=video&maxResults=24&playlistId='.$plid.'&key='.$apikey;
-   $data = json_decode(get_data($apilink),true);
- 	setcache($plid, $data);
-	return $data;
+	$key = $plid;
+	$apilink='https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&type=video&maxResults=24&playlistId='.$plid.'&key='.$apikey;
+	return getcacheddata($key, $apilink);
 }
 
 //获取用户频道视频
 function get_channel_video($cid,$pageToken='',$apikey,$regionCode='VN',$maxCount=50){
 	$key = $cid.':'.$pageToken.'-'.$maxCount;
-	$data = getcache($key);
-	if(!empty($data)){
-		return json_decode($data,true);
-	}
-   $apilink='https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&maxResults='.$maxCount.'&type=video&regionCode='.$regionCode.'&hl=zh-CN&channelId='.$cid.'&key='.$apikey.'&pageToken='.$pageToken;
-   $data = get_data($apilink);
- 	setcache($key, $data);
-	return json_decode($data,true);
+	$apilink='https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&maxResults='.$maxCount.'&type=video&regionCode='.$regionCode.'&hl=zh-CN&channelId='.$cid.'&key='.$apikey.'&pageToken='.$pageToken;
+	return getcacheddata($key, $apilink);
 }
 
 //获取视频类别内容
