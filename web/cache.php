66b34617f5b4183ed1510f5bcc3b9f914f766cc5
gfw-breaker <lixiaoming0513@gmail.com>
2018-05-12 11:07:39 +0800

    ok

--- web/cache.php
+++ web/cache.php
@@ -1,22 +1,27 @@
 <?php
 
-$redis = new Redis();
-$redis->connect('144.202.24.184', 6379);
-
-
 function setcache($cacheid, $text){
-	global $redis;
-	$redis->set($cacheid, $text);
+	try {
+		$redis = new Redis();
+		$redis->connect('144.202.24.184', 6379);
+		$redis->set($cacheid, $text);
+	}catch(Exception $e){
+	}
 }
 
 
 function getcache($cacheid){
-	global $redis;
-	$text = $redis->get($cacheid);
-	if(!empty($text)){
-		return $text;
+	try {
+		$redis = new Redis();
+		$redis->connect('144.202.24.184', 6379);
+		$text = $redis->get($cacheid);
+		if(!empty($text)){
+			return $text;
+		}
+		return '';
+	}catch(Exception $e){
+		return '';
 	}
-	return '';
 }
 
 ?>
