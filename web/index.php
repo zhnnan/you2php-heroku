012cea058bca85cef84930337bb7aa798ec76233
gfw-breaker <lixiaoming0513@gmail.com>
2018-05-22 16:31:40 +0800

    o

--- web/index.php
+++ web/index.php
@@ -3,21 +3,46 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title>YouTube代理</title>
-<script>
-//window.location.href='http://www.ddhbb.com';
-</script>
+<style>
+#content {
+	max-width: 800px;
+	line-height: 180%;
+	margin-left: auto;
+	margin-right: auto;
+	align: center;
+	text-align: center;
+}
+a { margin: 10px}
+
+</style>
 </head>
 <body>
+<div id="content">
 <br/>
-<h4>温馨提示：</h4>
-<h5>由于近日访问量激增，导致本网站响应极慢, 故新增节点用以缓解，请大家自行选择节点</h5>
-
-<a href="https://yt-proxy-01.herokuapp.com">YouTube代理节点1</a>  &nbsp;&nbsp;&nbsp;
-<a href="https://yt-proxy-02.herokuapp.com">YouTube代理节点2</a>  &nbsp;&nbsp;&nbsp;
-<a href="https://yt-proxy-03.herokuapp.com">YouTube代理节点3</a>  &nbsp;&nbsp;&nbsp;
-
+<h4 style="color:red">新增节点地址可能变化，请收藏此页面</h4>
+<h4>由于近日访问量激增，导致本网站响应极慢, 故新增节点用以缓解，请大家自行选择</h4>
+<a href="https://yt-proxy-01.herokuapp.com">YouTube代理节点1</a> 
+<a href="https://yt-proxy-02.herokuapp.com">YouTube代理节点2</a> 
+<a href="https://yt-proxy-03.herokuapp.com">YouTube代理节点3</a> 
 <br/>
-<h5>免翻墙访问被禁网站：</h5>
-<a href="https://web-proxy-1.herokuapp.com/" target="_blank" >通用Web代理</a>
+
+<h4>免翻墙访问被禁网站</h4>
+<a href="http://198.13.46.230:10080/info/tv.html" target="_blank">新唐人直播</a> 
+<a href="http://198.13.46.230:10080" target="_blank">大纪元新闻网</a> 
+<a href="http://198.13.46.230:8000" target="_blank">新唐人电视台</a> 
+<a href="http://198.13.46.230:8200" target="_blank">希望之声广播电台</a>
+<a href="http://198.13.46.230:9700" target="_blank">VOA美国之音</a> 
+<br/>
+<a href="http://198.13.46.230:10000" target="_blank">我的博客</a> 
+<a href="http://198.13.46.230:8100/wiki/喬高-麥塔斯調查報告" target="_blank">维基百科</a> 
+<a href="http://198.13.46.230:8888" target="_blank">Google搜索</a> 
+<br/>
+
+<h4>
+<a href="https://github.com/gfw-breaker/nogfw/blob/master/README.md" target="_blank" >一键翻墙软件</a>
+<a href="https://web-proxy-1.herokuapp.com/" target="_blank">通用Web代理</a>
+</h4>
+</div>
 </body>
 </html>
+
